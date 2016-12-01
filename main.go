95b45d0817057462ce46e263d422e07c984ec525
Ryan Harper <ryanharper007@zer0touch.co.uk>
2016-12-01 00:38:30 +0000

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
     "encoding/json"
-    "github.com/andlabs/ui"
+    //"github.com/andlabs/ui"
     "fmt"
     "io/ioutil"
     "os"
@@ -32,28 +32,28 @@ func toJson(p interface{}) string {
 }
 
 func main() {
-	err := ui.Main(func() {
-        name := ui.NewEntry()
-        button := ui.NewButton("Greet")
-        greeting := ui.NewLabel("")
-        box := ui.NewVerticalBox()
-        box.Append(ui.NewLabel("Enter your name:"), false)
-        box.Append(name, false)
-        box.Append(button, false)
-        box.Append(greeting, false)        window := ui.NewWindow("Hello", 200, 100, false)
-        window.SetChild(box)
-        button.OnClicked(func(*ui.Button) {
-            greeting.SetText("Hello, " + name.Text() + "!")
-        })
-        window.OnClosing(func(*ui.Window) bool {
-            ui.Quit()
-            return true
-        })
-        window.Show()
-    })
-    if err != nil {
-        panic(err)
-    }
+//	err := ui.Main(func() {
+ //       name := ui.NewEntry()
+  //      button := ui.NewButton("Greet")
+   //     greeting := ui.NewLabel("")
+    //    box := ui.NewVerticalBox()
+     //   box.Append(ui.NewLabel("Enter your name:"), false)
+      //  box.Append(name, false)
+       // box.Append(button, false)
+       // box.Append(greeting, false)        window := ui.NewWindow("Hello", 200, 100, false)
+       // window.SetChild(box)
+       // button.OnClicked(func(*ui.Button) {
+       //     greeting.SetText("Hello, " + name.Text() + "!")
+       // })
+       // window.OnClosing(func(*ui.Window) bool {
+       //     ui.Quit()
+       //     return true
+       // })
+      //  window.Show()
+   // })
+  //  if err != nil {
+ //       panic(err)
+ //   }
 
 
     //var ip = flag.Int("flagname", 1234, "help message for flagname")
